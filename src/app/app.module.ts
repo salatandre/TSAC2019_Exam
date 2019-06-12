e653138aa060f58be3684bf16ee63e0b01d3b89f
Andrea Salata <a.salata@mvlabs.it>
2019-06-12 23:33:26 +0200

    created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,16 @@ import { AppComponent } from "./app.component";
 import { BusDataComponent } from "./bus/bus-data/bus-data.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatTableModule } from "@angular/material/table";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, BusDataComponent],
+  declarations: [AppComponent, BusDataComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatTableModule,
+    MatToolbarModule,
     FormsModule,
     BrowserAnimationsModule
   ],
